953ca95ae10fce73b526998e45063961c6189683
Real Ghost <alejandro519@gmail.com>
2019-10-15 14:55:38 -0700

    Added styling to HTML files

---
+++ public/js/home.js
@@ -0,0 +1,12 @@
+// Name	Item Name	Item Price
+// Pizza Hut	Pizza	$5.00
+// Burger Master	cheese burger	$3.76
+// Rubios	taco	$2.00
+
+var restaurants = [
+  {
+    name: "Pizza Hut",
+    item: "pizza",
+    price: 5
+  }
+];
--- server.js
+++ server.js
@@ -25,12 +25,15 @@ require("./routes/htmlroutes.js")(app);
 require("./routes/apiroutes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
+db.sequelize
+  .sync({ force: true })
+  .then(function() {
+    app.listen(PORT, function() {
+      console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+    });
+  })
+  .catch(console.log);
