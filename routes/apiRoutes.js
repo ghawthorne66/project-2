5b359daecf6e4c60745ef18ec890903281d6a2e9
Greg Hawthorne <ghawthorne@me.com>
2019-10-15 14:40:51 -0700

    brought files over

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -36,6 +36,7 @@ module.exports = function(app) {
 
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", function(req, res) {
+    console.log(req.body)
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
